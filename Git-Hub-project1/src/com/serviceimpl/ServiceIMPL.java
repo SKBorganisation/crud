216ca8b04bfb3641c55ec95f9c6f677b6346a41a
cjcbhagyashri <shri28394@gmail.com>
2018-10-24 12:57:06 +0530

    change in login page

--- Git-Hub-project1/src/com/serviceimpl/ServiceIMPL.java
+++ Git-Hub-project1/src/com/serviceimpl/ServiceIMPL.java
@@ -1,5 +1,10 @@
 package com.serviceimpl;
 
-public class ServiceIMPL {
+import org.springframework.stereotype.Service;
+
+import com.serviceinterface.ServiceInterface;
+
+@Service
+public class ServiceIMPL implements ServiceInterface{
 
 }
