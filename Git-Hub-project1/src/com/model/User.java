8a23aed4f619cd0fea9fedee22f39ff30434beaa
cjcbhagyashri <shri28394@gmail.com>
2018-10-24 13:52:07 +0530

    User change

--- Git-Hub-project1/src/com/model/User.java
+++ Git-Hub-project1/src/com/model/User.java
@@ -2,12 +2,16 @@ package com.model;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 @Entity
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int uid;
 	private String fname;
 	private String lname;
