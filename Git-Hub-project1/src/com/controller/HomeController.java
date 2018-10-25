6f4eac05e050cc0afb1b47c6e042751f4e5328ad
sonikatakalkar08 <sonikatakalkar10@gmail.com>
2018-10-25 10:39:52 +0530

    change in home controller

--- Git-Hub-project1/src/com/controller/HomeController.java
+++ Git-Hub-project1/src/com/controller/HomeController.java
@@ -1,9 +1,21 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	//hiiii
+
+	@RequestMapping("/")
+	public String loginPgae()
+			{
+				return "login";
+			}
+
+	@RequestMapping("/registerPage")
+	public String registerPage()
+	{
+		return "register";
+	}
 
 }
